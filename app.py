f4f2e71319ca55ebc6bd2fec214ca955115ff335
Denis Sevosteenko <den.sev@outlook.com>
2017-08-28 12:50:55 +0300

    flask stuff

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from tkinter import *
+from flask import Flask, jsonify
 
-window = Tk()
-#window
-#lb = Label(window, text = " test text adsfasdfasdfasdfsadf")
-#lb.pack()
-window.mainloop()
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
 
 
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
